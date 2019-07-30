ff4f287dc3a3e8b1859183a446427d5148a68a61
BlandineK <blandineuwase222@gmail.com>
2019-07-30 12:20:58 +0200

    add new stylings

--- js/java.js
+++ js/java.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
-$("#design").click(function(){
-   $("#design").hide();
-   $("#a").show();
-});
-$("#develo").click(function(){
-   $("#develo").hide();
-   $("#b").show();
-});
-$("#product").click(function(){
-   $("#product").hide();
-   $("#c").show();
-});
+  $("#design").click(function(){
+    $("#design").hide();
+    $("#a").show();
+  });
+  $("#a").click(function(){
+    $("#a").hide();
+    $("#design").show();
+  });
+  $("#develo").click(function(){
+    $("#develo").hide();
+    $("#b").show();
+  });
+  $("#b").click(function(){
+    $("#b").hide();
+    $("#develo").show();
+  });
+  $("#product").click(function(){
+    $("#product").hide();
+    $("#c").show();
+  });
+  $("#c").click(function(){
+    $("#c").hide();
+    $("#product").show();
+  });
 });
